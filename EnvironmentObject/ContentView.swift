b9a89d78284151f6ff5c0da5d086316a557373cf
Cengizhan Tomak <cengizhantomak@gmail.com>
2023-09-17 19:52:43 +0300

    Initial Commit

---
+++ EnvironmentObject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EnvironmentObject
+//
+//  Created by Kerem Tuna Tomak on 17.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EnvironmentObject/EnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectApp.swift
+//  EnvironmentObject
+//
+//  Created by Kerem Tuna Tomak on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
